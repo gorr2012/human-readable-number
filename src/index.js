f8145b77751352b41a8ecd1fa7f073d27719aa30
gorr2012 <sergey_gornost@mail.ru>
2021-01-16 16:36:26 +0300

    refactor: formatting code

--- src/index.js
+++ src/index.js
@@ -5,33 +5,23 @@ module.exports = function toReadable (number) {
 	let arrAfterTwenty = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
 
 	let hundreds = '';
-	let tens = '';
-    let ones = '';
+	let dozens = '';
+    let units = '';
 
 	for(let i = 1; i < arrToTwenty.length; i++){
-		if( ( number % 100 === i && number % 100 < 20 )  || ( number % 10 === i && number % 10 <= 10 )){
-			ones = arrToTwenty[i];
-		}
-		
-		if((number - number % 100) / 100 === i && number % 10 < 10){
-			hundreds = arrToTwenty[i] + ' hundred';
-		}
+		if( ( number % 100 === i && number % 100 < 20 )  || ( number % 10 === i && number % 10 <= 10 ))	units = arrToTwenty[i];
+		if( (number - number % 100) / 100 === i && number % 10 < 10)	hundreds = arrToTwenty[i] + ' hundred';
 	}
 
 	for(let i = 1; i < arrAfterTwenty.length+2; i++){
-		if((number % 100 - number % 10)/10 === i && number % 100 >= 20){
-			tens = arrAfterTwenty[i-2];
-		}
+		if((number % 100 - number % 10)/10 === i && number % 100 >= 20)	dozens = arrAfterTwenty[i-2];
     }
 
-    if(number < 20) return `${ones}`;
-
-    if(number < 100 && number >= 20 && number % 10 === 0) return `${tens}`;
-    if(number < 100 && number >= 20 && number % 10 !== 0) return `${tens} ${ones}`;
-
+    if(number < 20) return `${units}`;
+    if(number < 100 && number >= 20 && number % 10 === 0) return `${dozens}`;
+    if(number < 100 && number >= 20 && number % 10 !== 0) return `${dozens} ${units}`;
     if(number % 100 === 0 && number >= 100) return `${hundreds}`;
-    if(number % 10 === 0 && number >= 100 && number % 100 !== 10) return `${hundreds} ${tens}`;
-    if(number % 100 < 20 && number >= 100 && number % 100 !== 0) return `${hundreds} ${ones}`;
-
-    if(number >= 100) return `${hundreds} ${tens} ${ones}`;
+    if(number % 10 === 0 && number >= 100 && number % 100 !== 10) return `${hundreds} ${dozens}`;
+    if(number % 100 < 20 && number >= 100 && number % 100 !== 0) return `${hundreds} ${units}`;
+    if(number >= 100) return `${hundreds} ${dozens} ${units}`;
 }
